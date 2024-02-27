e0d766b9b8f24805869cd40df4555f653b16e301
shabbir <kaderishabbir@gmail.com>
2024-02-27 19:28:22 +0530

    Updated Arduino Code

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,14 +1,14 @@
 #include <DS3231.h>
-#include <DFRobot_sim808.h>
+// #include <DFRobot_sim808.h>
 #include <SoftwareSerial.h>
 #include <SD.h>
 #include "ELMduino.h"
 #include <MPU6050.h>
 
 
-DS3231 rtc(2, 3);
+// DS3231 rtc(2, 3);
 SoftwareSerial mySerial(10, 11); //sim808 serial
-DFRobot_SIM808 sim808(&mySerial);  //Connect RX,TX,PWR, 
+// DFRobot_SIM808 sim808(&mySerial);  //Connect RX,TX,PWR, 
 SoftwareSerial obdSerial(0,1); // RX, TX obd
 const int chipSelect = 53;
 const int sensorPin = A0;  // analog input pin for the sensor
@@ -27,7 +27,7 @@ int messageIndex = 0;
 char phone[16];
 char datetime[24];
 String date;
-String time;
+String ztime;
 char MESSAGE[300];
 float lat, lon, speed;
 ELM327 myELM327;
@@ -40,51 +40,51 @@ int gx, gy, gz;
 
 
 void setup() {
-  rtc.begin();
-  mySerial.begin(9600);
+  // rtc.begin();
+  // mySerial.begin(9600);
   Serial.begin(9600);
 
   //******** Initialize SD Card module *************
-  if (!SD.begin(chipSelect)) {
-    Serial.println("SD card initialization failed");
-    return;
-  }
-  Serial.println("SD card initialized");
+  // if (!SD.begin(chipSelect)) {
+  //   Serial.println("SD card initialization failed");
+  //   // return;
+  // }
+  // Serial.println("SD card initialized");
 
   //******** Initialize sim808 module *************
-  while (!sim808.init()) {
-    delay(2000);
+  // while (!sim808.init()) {
+  //   delay(2000);
 
-    Serial.print("Sim808 init error\r\n");
-  }
-  Serial.println("Sim808 init success");
+  //   Serial.print("Sim808 init error\r\n");
+  // }
+  // Serial.println("Sim808 init success");
 
   //******** Set Date and Time *************
-  rtc.setDate(16, 6, 2023);
-  rtc.setTime(12, 19, 30);
+  // rtc.setDate(16, 6, 2023);
+  // rtc.setTime(12, 19, 30);
 
   //************* Turn on the GPS power************
-  if (sim808.attachGPS()){
-    Serial.println("Open the GPS power success");
+//   if (sim808.attachGPS()){
+//     Serial.println("Open the GPS power success");
     
-}
-  else
-    Serial.println("Open the GPS power failure");
+// }
+//   else
+//     Serial.println("Open the GPS power failure");
 
 
 
-  //******** Initialize ELM327 module *************
- if (!myELM327.begin(ELM_PORT, true, 2000))
- {
-   Serial.println("Couldn't connect to OBD scanner");
- }
+//   //******** Initialize ELM327 module *************
+//  if (!myELM327.begin(ELM_PORT, true, 2000))
+//  {
+//    Serial.println("Couldn't connect to OBD scanner");
+//  }
 
- Serial.println("Connected to ELM327");
+//  Serial.println("Connected to ELM327");
 
 //******** Initialize Gyroscope module *************
   mpu.initialize();
   
-  Serial.println("MPU-6050 Test");
+  Serial.println("GEN-Z DriveInsights MPU-6050 Test");
   
   Serial.println("Initializing MPU6050...");
   mpu.initialize();
@@ -95,98 +95,6 @@ void setup() {
   delay(1000);
 }
 
-
-
-
-void loop() {
-  if (sim808.getGPS()) {
-    getGPSData();
-    getECUData();
-    delay(2000);
-    sensorValue = analogRead(sensorPin);
-    gyro();
-    date = rtc.getDateStr();
-    time = rtc.getTimeStr();
-    Serial.print("date: ");
-    Serial.println(date);
-    Serial.print("time: ");
-    Serial.println(time);
-    messageIndex = sim808.isSMSunread();
-    Serial.print("messageIndex: ");
-    Serial.println(messageIndex);
-
-
-  dtostrf(lat, 10, 7, latStr);  // convert the latitude float to a string with 7 decimal places
-  dtostrf(lon, 10, 7, lonStr);  // convert the longitude float to a string with 7 decimal places
-if(sensorValue>threshold){
-  sosTriger();
-}
-    upload();
-  }
-}
-
-void getECUData(){
-  float tempRPM = myELM327.rpm();
-  float tempFuel = myELM327.fuelLevel();
-
- if (myELM327.nb_rx_state == ELM_SUCCESS)
- {
-   rpm = (int)tempRPM;
-   fuel = (float)tempFuel;
-   Serial.print("RPM: "); Serial.println(rpm);
-   Serial.print("Fuel Level: "); Serial.println(fuel);
- }
- else if (myELM327.nb_rx_state != ELM_GETTING_MSG)
-   myELM327.printError();
-}
-
-void sosTriger(){
-  String MESSAGE = "Impact Detected! https://www.google.com/maps/search/?api=1&query=" + String(latStr) + "," + String(lonStr);
-  MESSAGE.replace("query= ", "query=");
-  sim808.sendSMS(PHONE_NUMBER1, MESSAGE.c_str());
-  sim808.sendSMS(PHONE_NUMBER2, MESSAGE.c_str());
-  Serial.print(MESSAGE);
-}
-
-void fileWrite() {
-  File dataFile = SD.open("data.csv", FILE_WRITE);
-
-  if (dataFile) {
-    dataFile.print(date);
-    dataFile.print(",");
-    dataFile.print(time);
-    dataFile.print(",");
-    dataFile.print(latStr);  
-    dataFile.print(",");  
-    dataFile.print(lonStr); 
-    dataFile.print(",");
-    dataFile.print(speed); 
-    dataFile.print(",");
-    dataFile.print(rpm); 
-    dataFile.print(",");
-    dataFile.print(fuel); 
-    dataFile.print(",");
-    dataFile.print(ax); 
-    dataFile.print(",");
-    dataFile.print(ay); 
-    dataFile.print(",");
-    dataFile.print(az); 
-    dataFile.print(",");
-    dataFile.print(gx); 
-    dataFile.print(",");
-    dataFile.print(gy); 
-    dataFile.print(",");
-    dataFile.print(gz); 
-    dataFile.print(",");
-    dataFile.print(sensorValue);
-    dataFile.print(",");
-    dataFile.println();  // end the line
-    dataFile.close();
-    Serial.println("Data written to file");
-  } else {
-    Serial.println("Error opening file");
-  }
-}
 void gyro(){
   mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
   
@@ -204,128 +112,219 @@ void gyro(){
   
 }
 
-void upload() {
 
-  fileWrite();
-  if (mySerial.available())
-    Serial.write(mySerial.read());
-
-  mySerial.println("AT");
-  delay(1000);
-
-  mySerial.println("AT+CPIN?");
-  delay(1000);
-
-  mySerial.println("AT+CREG?");
-  delay(1000);
-
-  mySerial.println("AT+CGATT?");
-  delay(1000);
-
-  mySerial.println("AT+CIPSHUT");
-  delay(1000);
-
-  mySerial.println("AT+CIPSTATUS");
-  delay(2000);
-
-  mySerial.println("AT+CIPMUX=0");
-  delay(2000);
-
-  ShowSerialData();
-
-  mySerial.println("AT+CSTT=\"dialogbb\"");  //start task and setting the APN,
-  delay(1000);
-
-  ShowSerialData();
-
-  mySerial.println("AT+CIICR");  //bring up wireless connection
-  delay(3000);
-
-  ShowSerialData();
-
-  mySerial.println("AT+CIFSR");  //get local IP adress
-  delay(2000);
-
-  ShowSerialData();
-
-  mySerial.println("AT+CIPSPRT=0");
-  delay(3000);
-
-  ShowSerialData();
-
-  mySerial.println("AT+CIPSTART=\"TCP\",\"api.thingspeak.com\",\"80\"");  //start up the connection
-  delay(2000);
-
-  ShowSerialData();
-
-  mySerial.println("AT+CIPSEND");  //begin send data to remote server
-  delay(4000);
-  ShowSerialData();
-
-  String str = "GET https://api.thingspeak.com/update?api_key=DTUJH2YW8QKCOG4F&field1=" + String(latStr) + "&field2=" + String(lonStr) + "&field3=" + time + "&field4=" + date + "&field5=" + speed+ "&field6=" + sensorValue+ "&field7=" + rpm+"&field8=" + fuel;
-  str.replace("field1= ", "field1=");  // remove the space between the latitude/longitude and "&field3"
-
-  Serial.println(str);
-  mySerial.println(str);  //begin send data to remote server
-
-  delay(4000);
-  ShowSerialData();
-
-  mySerial.println((char)26);  //sending
-  delay(2000);                 //waitting for reply, important! the time is base on the condition of internet
-  mySerial.println();
-
-  ShowSerialData();
-
-  mySerial.println("AT+CIPSHUT");  //close the connection
-  delay(100);
-  ShowSerialData();
-  delay(2000);
-
-  if (messageIndex > 0) {
-    sim808.readSMS(messageIndex, message, MESSAGE_LENGTH, phone, datetime);
-    sim808.deleteSMS(messageIndex);
-
-    Serial.println("AT+CMGF=1");
+void loop() {
+  // if (sim808.getGPS()) {
+    // getGPSData();
+    // getECUData();
     delay(2000);
-    Serial.print("AT+CMGDA=");
-    Serial.write(34);
-    Serial.print("DEL ALL");
-    Serial.write(34);
-    Serial.write(13);
-    Serial.write(10);
-    msg();
-  }
+    // sensorValue = analogRead(sensorPin);
+    gyro();
+    // date = rtc.getDateStr();
+    // time = rtc.getTimeStr();
+    // Serial.print("date: ");
+    // Serial.println(date);
+    // Serial.print("time: ");
+    // Serial.println(time);
+    // messageIndex = sim808.isSMSunread();
+    // Serial.print("messageIndex: ");
+    // Serial.println(messageIndex);
+
+
+  // dtostrf(lat, 10, 7, latStr);  // convert the latitude float to a string with 7 decimal places
+  // dtostrf(lon, 10, 7, lonStr);  // convert the longitude float to a string with 7 decimal places
+  // if(sensorValue>threshold){
+  //   sosTriger();
+  // }
+    // upload();
+  // }
 }
 
+// void getECUData(){
+//   float tempRPM = myELM327.rpm();
+//   float tempFuel = myELM327.fuelLevel();
+
+//  if (myELM327.nb_rx_state == ELM_SUCCESS)
+//  {
+//    rpm = (int)tempRPM;
+//    fuel = (float)tempFuel;
+//    Serial.print("RPM: "); Serial.println(rpm);
+//    Serial.print("Fuel Level: "); Serial.println(fuel);
+//  }
+//  else if (myELM327.nb_rx_state != ELM_GETTING_MSG)
+//    myELM327.printError();
+// }
+
+// void sosTriger(){
+//   String MESSAGE = "Impact Detected! https://www.google.com/maps/search/?api=1&query=" + String(latStr) + "," + String(lonStr);
+//   MESSAGE.replace("query= ", "query=");
+//   sim808.sendSMS(PHONE_NUMBER1, MESSAGE.c_str());
+//   sim808.sendSMS(PHONE_NUMBER2, MESSAGE.c_str());
+//   Serial.print(MESSAGE);
+// }
+
+// void fileWrite() {
+//   File dataFile = SD.open("data.csv", FILE_WRITE);
+
+//   if (dataFile) {
+//     dataFile.print(date);
+//     dataFile.print(",");
+//     dataFile.print(time);
+//     dataFile.print(",");
+//     dataFile.print(latStr);  
+//     dataFile.print(",");  
+//     dataFile.print(lonStr); 
+//     dataFile.print(",");
+//     dataFile.print(speed); 
+//     dataFile.print(",");
+//     dataFile.print(rpm); 
+//     dataFile.print(",");
+//     dataFile.print(fuel); 
+//     dataFile.print(",");
+//     dataFile.print(ax); 
+//     dataFile.print(",");
+//     dataFile.print(ay); 
+//     dataFile.print(",");
+//     dataFile.print(az); 
+//     dataFile.print(",");
+//     dataFile.print(gx); 
+//     dataFile.print(",");
+//     dataFile.print(gy); 
+//     dataFile.print(",");
+//     dataFile.print(gz); 
+//     dataFile.print(",");
+//     dataFile.print(sensorValue);
+//     dataFile.print(",");
+//     dataFile.println();  // end the line
+//     dataFile.close();
+//     Serial.println("Data written to file");
+//   } else {
+//     Serial.println("Error opening file");
+//   }
+// }
+
+// void upload() {
+
+//   fileWrite();
+//   if (mySerial.available())
+//     Serial.write(mySerial.read());
+
+//   mySerial.println("AT");
+//   delay(1000);
+
+//   mySerial.println("AT+CPIN?");
+//   delay(1000);
+
+//   mySerial.println("AT+CREG?");
+//   delay(1000);
+
+//   mySerial.println("AT+CGATT?");
+//   delay(1000);
+
+//   mySerial.println("AT+CIPSHUT");
+//   delay(1000);
+
+//   mySerial.println("AT+CIPSTATUS");
+//   delay(2000);
+
+//   mySerial.println("AT+CIPMUX=0");
+//   delay(2000);
+
+//   ShowSerialData();
+
+//   mySerial.println("AT+CSTT=\"dialogbb\"");  //start task and setting the APN,
+//   delay(1000);
+
+//   ShowSerialData();
+
+//   mySerial.println("AT+CIICR");  //bring up wireless connection
+//   delay(3000);
+
+//   ShowSerialData();
+
+//   mySerial.println("AT+CIFSR");  //get local IP adress
+//   delay(2000);
+
+//   ShowSerialData();
+
+//   mySerial.println("AT+CIPSPRT=0");
+//   delay(3000);
+
+//   ShowSerialData();
+
+//   mySerial.println("AT+CIPSTART=\"TCP\",\"api.thingspeak.com\",\"80\"");  //start up the connection
+//   delay(2000);
+
+//   ShowSerialData();
+
+//   mySerial.println("AT+CIPSEND");  //begin send data to remote server
+//   delay(4000);
+//   ShowSerialData();
+
+//   String str = "GET https://api.thingspeak.com/update?api_key=DTUJH2YW8QKCOG4F&field1=" + String(latStr) + "&field2=" + String(lonStr) + "&field3=" + time + "&field4=" + date + "&field5=" + speed+ "&field6=" + sensorValue+ "&field7=" + rpm+"&field8=" + fuel;
+//   str.replace("field1= ", "field1=");  // remove the space between the latitude/longitude and "&field3"
+
+//   Serial.println(str);
+//   mySerial.println(str);  //begin send data to remote server
+
+//   delay(4000);
+//   ShowSerialData();
+
+//   mySerial.println((char)26);  //sending
+//   delay(2000);                 //waitting for reply, important! the time is base on the condition of internet
+//   mySerial.println();
+
+//   ShowSerialData();
+
+//   mySerial.println("AT+CIPSHUT");  //close the connection
+//   delay(100);
+//   ShowSerialData();
+//   delay(2000);
+
+//   if (messageIndex > 0) {
+//     sim808.readSMS(messageIndex, message, MESSAGE_LENGTH, phone, datetime);
+//     sim808.deleteSMS(messageIndex);
+
+//     Serial.println("AT+CMGF=1");
+//     delay(2000);
+//     Serial.print("AT+CMGDA=");
+//     Serial.write(34);
+//     Serial.print("DEL ALL");
+//     Serial.write(34);
+//     Serial.write(13);
+//     Serial.write(10);
+//     msg();
+//   }
+// }
 
 
-void getGPSData() {
-  lat = (sim808.GPSdata.lat);
-  lon = (sim808.GPSdata.lon);
-  speed = sim808.GPSdata.speed_kph;
 
-  Serial.print("latitude :");
-  Serial.println(lat, 5);
-  Serial.print("longitude :");
-  Serial.println(lon, 5);
-  Serial.print("speed_kph :");
-  Serial.println(speed, 5);
-}
+// void getGPSData() {
+//   lat = (sim808.GPSdata.lat);
+//   lon = (sim808.GPSdata.lon);
+//   speed = sim808.GPSdata.speed_kph;
 
-
-
-void msg() {
-  String MESSAGE = "https://www.google.com/maps/search/?api=1&query=" + String(latStr) + "," + String(lonStr);
-  MESSAGE.replace("query= ", "query=");
-  sim808.sendSMS(PHONE_NUMBER1, MESSAGE.c_str());
-  sim808.sendSMS(PHONE_NUMBER2, MESSAGE.c_str());
-  Serial.print(MESSAGE);
-}
+//   Serial.print("latitude :");
+//   Serial.println(lat, 5);
+//   Serial.print("longitude :");
+//   Serial.println(lon, 5);
+//   Serial.print("speed_kph :");
+//   Serial.println(speed, 5);
+// }
 
 
-void ShowSerialData() {
-  while (mySerial.available() != 0)
-    Serial.write(mySerial.read());
-  delay(1000);
-}
+
+// void msg() {
+//   String MESSAGE = "https://www.google.com/maps/search/?api=1&query=" + String(latStr) + "," + String(lonStr);
+//   MESSAGE.replace("query= ", "query=");
+//   sim808.sendSMS(PHONE_NUMBER1, MESSAGE.c_str());
+//   sim808.sendSMS(PHONE_NUMBER2, MESSAGE.c_str());
+//   Serial.print(MESSAGE);
+// }
+
+
+// void ShowSerialData() {
+//   while (mySerial.available() != 0)
+//     Serial.write(mySerial.read());
+//   delay(1000);
+// }
